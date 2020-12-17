bab35d5095ef3148aaa96f61673c79202165663d
Filip Zoric <filip.zoric@gideonbros.ai>
2020-12-17 11:20:02 +0100

    Added Qt build in dockerfile and configuration script

---
+++ captoglove/configuration.sh
@@ -0,0 +1,48 @@
+  GNU nano 2.5.3                       File: configuration.sh                                                      
+
+./configure -prefix /home/Qt \
+            -skip qt3d \
+            -skip qtactiveqt \
+            -skip qtandroidextras \
+            -skip qtdatavis3d \
+            -skip qtdeclarative \
+            -skip qtgamepad \
+            -skip qtgraphicaleffects \
+            -skip qtimageformats \
+            -skip qtlocation \
+            -skip qtlottie \
+            -skip qtmacextras \
+            -skip qtmultimedia \
+            -skip qtpurchasing \
+            -skip qtquick3d \
+            -skip qtquickcontrols \
+            -skip qtquickcontrols2 \
+            -skip qtquicktimeline \
+            -skip qtremoteobjects \
+            -skip qtscript \
+            -skip qtscxml \
+            -skip qtspeech \
+            -skip qtsvg \
+            -skip qttranslations \
+            -skip qtvirtualkeyboard \
+            -skip qtwayland \
+            -skip qtwebchannel \
+            -skip qtwebengine \
+            -skip qtwebglplugin \
+            -skip qtwebsockets \
+            -skip qtwebview \
+            -skip qtwinextras \
+            -skip qtxmlpatterns
+
+
+
+
+
+
+
+
+
+
+
+
+
