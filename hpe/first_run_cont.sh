4983e65eabfec4ca1c531da9cc70cfbea2767ad2
Filip Zoric <filip.zoric@fer.hr>
2020-12-26 18:35:11 +0100

    Captoglove ENV var fix and hpe development source image

---
+++ hpe/first_run_cont.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash 
+
+# export DATA_PATH=$PWD in correct folder /home/Work/data
+docker run -it --network host --privileged --gpus all \
+       -e DISPLAY=$DISPLAY \
+       -v /dev:/dev \
+       -v /tmp/.X11-unix:/tmp/.X11-unix \
+       -v $DATA_PATH:/home/developer/data \
+       --name hpe_cont hpe_img:latest
