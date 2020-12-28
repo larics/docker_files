2ad85184f646041035d9820a797b7400451b0b42
Filip Zoric <filip.zoric@fer.hr>
2020-12-28 19:01:08 +0100

    Added some additional packages to hpe

--- hpe/first_run_cont.sh
+++ hpe/first_run_cont.sh
@@ -6,4 +6,4 @@ docker run -it --network host --privileged --gpus all \
        -v /dev:/dev \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        -v $DATA_PATH:/home/developer/data \
-       --name hpe_cont hpe_img:latest
+       --name hpe_cont hpe_full_img:latest
