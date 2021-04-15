ae05c9c2b56c5a4d41037b4b7230feee00e31ac2
Filip Zoric <fzoric8@gmail.com>
2021-04-15 15:44:36 +0200

    Added glvnd version change to match newest NVIDIA drivers and rqt pack installation

--- ros-melodic/dynamixel/first_run.sh
+++ ros-melodic/dynamixel/first_run.sh
@@ -4,4 +4,4 @@ docker run -it --network host --privileged --gpus all \
        -e DISPLAY=$DISPLAY \
        -v /dev:/dev \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
-       --name dynamixel_cont2 dynamixel_img:melodic
+       --name dynamixel_cont dynamixel_img:latest
