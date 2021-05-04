8728c7512112973a1c76b7435db83679e9312eb3
Filip Zoric <fzoric8@gmail.com>
2021-05-04 16:44:20 +0200

    Added initial audio support for original mmuav_img

---
+++ ros-melodic/mmuav_audio_ros/first_run.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# If not working, first do: sudo rm -rf /tmp/.docker.xauth
+# It still not working, try running the script as root.
+
+XAUTH=/tmp/.docker.xauth
+
+echo "Preparing Xauthority data..."
+xauth_list=$(xauth nlist :0 | tail -n 1 | sed -e 's/^..../ffff/')
+if [ ! -f $XAUTH ]; then
+    if [ ! -z "$xauth_list" ]; then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+echo "Done."
+echo ""
+echo "Verifying file contents:"
+file $XAUTH
+echo "--> It should say \"X11 Xauthority data\"."
+echo ""
+echo "Permissions:"
+ls -FAlh $XAUTH
+echo ""
+echo "Running docker..."
+
+docker run -it \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix" \
+    --volume="/dev:/dev" \
+    --volume="/dev/snd:/dev/snd" \
+    --volume="/etc/machine-id:/etc/machine-id" \
+    --volume="/run/user/$uid/pulse:/run/user/$uid/pulse" \
+    --volume="/var/lib/dbus:/var/lib/dbus" \
+    --device /dev/snd \
+    --net=host \
+    --privileged \
+    --gpus all \
+    --name mmuav_audio_cont \
+    mmuav_audio_img:latest
+
+
+
