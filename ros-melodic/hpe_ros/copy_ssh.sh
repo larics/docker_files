9ce6230bd94a6064f5e742e7ba3e2f767f1e20bb
Filip Zoric <fzoric8@gmail.com>
2021-10-26 16:23:39 +0200

    Squashed commit of the following:
    commit c06aa5bd0950aff83ab62296f04f4c36c9c25cf0
    Merge: 4270eca ba3de10
    Author: Filip Zoric <fzoric8@gmail.com>
    Date:   Tue Oct 26 16:15:44 2021 +0200
    
        Merge branch 'master' of github.com:larics/docker_files
    
    commit 4270eca3d83116351071c09020b328804e3190e0
    Author: Filip Zoric <fzoric8@gmail.com>
    Date:   Tue Oct 26 16:10:15 2021 +0200
    
        Removed weights due to git file size limit
    
    commit 02741ff39320409d6fb108aa12ecdd8339430484
    Author: Filip Zoric <fzoric8@gmail.com>
    Date:   Thu Aug 26 12:22:42 2021 +0200
    
        Added run script for Blender 2.79
    
        Updates to hpe_ros container -> plug&play version
    
    commit ba3de1077f842a641e296553e51c6354844fdad0
    Merge: 9543a85 54583c0
    Author: Filip Zoric <fzoric8@gmail.com>
    Date:   Tue Oct 26 13:52:48 2021 +0200
    
        Merged pull
    
    commit 54583c059ae25bf244a1dbe369a1f4d912d4e508
    Merge: be60ec6 2fe4baa
    Author: Filip Zorić <fzoric8@gmail.com>
    Date:   Tue Aug 31 13:14:37 2021 +0200
    
        Merge pull request #5 from fzoric8/master
    
        Fetching upstream
    
    commit 2fe4baa3caa71feb931911240eec7548cd0b655b
    Author: Filip Zoric <fzoric8@gmail.com>
    Date:   Thu Aug 26 12:22:42 2021 +0200
    
        Added run script for Blender 2.79
    
    commit b2413dcf21ef1be68ab795ea650e6e1d997a8cbe
    Author: Filip Zoric <fzoric8@gmail.com>
    Date:   Thu Aug 26 12:22:09 2021 +0200
    
        Added blender 2.79 Dockerfile
    
    commit 5a1680e0f2ab5912451a03c97d8bd05785966cf0
    Author: Filip Zoric <fzoric8@gmail.com>
    Date:   Mon Aug 23 18:14:50 2021 +0200
    
        Added new Dockerfile with fixed SSH download
    
    commit 583325ba093df078b34d44db978bc996676224f8
    Author: Filip Zoric <fzoric8@gmail.com>
    Date:   Wed Aug 18 15:58:33 2021 +0200
    
        Added updated Dockerfile with all neccessary ROS pacakges for schunk manipulation
    
    commit 68a94fcf756f5ef5840dbe3b1be92e25cb6afbf9
    Author: Filip Zoric <fzoric8@gmail.com>
    Date:   Wed Aug 18 15:04:43 2021 +0200
    
        Added separator_end_effector control for schunk
    
    commit 9750752e0c6220a5c70dfc98871faa20fb0cc822
    Author: Filip Zoric <fzoric8@gmail.com>
    Date:   Wed Aug 18 14:59:07 2021 +0200
    
        Added a lot of updates for realrobot configuratioN
    
    commit 18cbcf2fab48ef9bfe0697851815e7c29e789de5
    Author: Filip Zoric <fzoric8@gmail.com>
    Date:   Thu Aug 12 18:07:42 2021 +0200
    
        Added initial noetic moveit ros docker
    
    commit 80acf33835843efe7c9faf48badadb5539190c72
    Merge: e57efbe 602e932
    Author: Filip Zoric <fzoric8@gmail.com>
    Date:   Wed Aug 11 12:28:59 2021 +0200
    
        Merge branch 'master' of https://github.com/fzoric8/docker_files
    
    commit e57efbea8113b1ae72d2691b1aef6e97d09b6ffe
    Author: Filip Zoric <fzoric8@gmail.com>
    Date:   Wed Aug 11 12:28:38 2021 +0200
    
        Updated README.md
    
    commit 602e93249f1010ccbd44e7bfe58806fd0a18e2fc
    Merge: b235055 be60ec6
    Author: Filip Zorić <fzoric8@gmail.com>
    Date:   Tue Jul 6 13:49:59 2021 +0200
    
        Merge branch 'larics:master' into master
    
    commit b2350550fcfcc2bc402eb493549b9c0cb219419f
    Author: Filip Zoric <fzoric8@gmail.com>
    Date:   Tue Jun 8 17:40:12 2021 +0200
    
        Updated deepspeech

---
+++ ros-melodic/hpe_ros/copy_ssh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "What's the name of the container where you want to copy ssh keys?"
+
+read cont_name
+
+echo "Container name where ssh_config is copied is: $cont_name"
+
+docker cp /home/$USER/.ssh/id_rsa $cont_name:/root/.ssh
+
