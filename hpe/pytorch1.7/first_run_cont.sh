dc9b47cc6b8807ee2afedaee98eb5afa52c1d2f6
Filip Zoric <fzoric8@gmail.com>
2021-12-14 16:28:38 +0100

    Added some stuff for ROSCONSOLE verbose output

--- hpe/pytorch1.7/first_run_cont.sh
+++ hpe/pytorch1.7/first_run_cont.sh
@@ -5,5 +5,4 @@ docker run -it --network host --privileged --gpus all \
        -e DISPLAY=$DISPLAY \
        -v /dev:/dev \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
-       -v $DATA_PATH:/home/developer/data \
        --name hpe_cont hpe_full_img:latest
