f2690e9d299aa8f8cc6844192cf95f15b3f572d6
Marijana Peti <marijana.peti@fer.hr>
2022-11-28 17:19:11 +0100

    Add Bluetooth drivres

---
+++ ros-noetic/mrs_course_project/to_copy/docker_entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#service dbus start
+#bluetoothd &
+
+rfkill block bluetooth
+service bluetooth stop
+rfkill unblock bluetooth
+service bluetooth start 
+
+/bin/bash
+cd /home/developer
