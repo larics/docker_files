2b619fa286649ac3f11c75affb49d494a5f4ec9d
aivano <antun.ivanovic@fer.hr>
2023-02-24 10:35:47 +0100

    [VirtualUavBag] Add bag with video to container.

--- ros-melodic/virtual_uav_bag/entrypoint.sh
+++ ros-melodic/virtual_uav_bag/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 bash -c "source /opt/ros/melodic/setup.bash; source /root/catkin_ws/devel/setup.bash; roslaunch uav_ros_bridge virtual_uav_mqtt.launch" & \
-bash -c "sleep 2; source /opt/ros/melodic/setup.bash; source /root/catkin_ws/devel/setup.bash; rosbag play -l virtual_uav_minimal.bag" 
+bash -c "sleep 4; source /opt/ros/melodic/setup.bash; source /root/catkin_ws/devel/setup.bash; rosbag play -l virtual_uav_minimal.bag" & \
+bash -c "sleep 4; source /opt/ros/melodic/setup.bash; source /root/catkin_ws/devel/setup.bash; rosbag play -l virtual_uav_video.bag"
