582db75a946db19fcb8b125f7ab6c8ebed550f8a
aivano <antun.ivanovic@fer.hr>
2023-02-27 09:22:01 +0100

    [VirtualUavBag] Add qr code scanner.

--- ros-melodic/virtual_uav_bag/entrypoint.sh
+++ ros-melodic/virtual_uav_bag/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 bash -c "source /opt/ros/melodic/setup.bash; source /root/catkin_ws/devel/setup.bash; roslaunch uav_ros_bridge virtual_uav_mqtt.launch" & \
 bash -c "sleep 4; source /opt/ros/melodic/setup.bash; source /root/catkin_ws/devel/setup.bash; rosbag play -l virtual_uav_minimal.bag" & \
-bash -c "sleep 4; source /opt/ros/melodic/setup.bash; source /root/catkin_ws/devel/setup.bash; rosbag play -l virtual_uav_video.bag"
+bash -c "sleep 4; source /opt/ros/melodic/setup.bash; source /root/catkin_ws/devel/setup.bash; rosbag play -l virtual_uav_video.bag" & \
+bash -c "sleep 8; source /opt/ros/melodic/setup.bash; source /root/catkin_ws/devel/setup.bash; rosrun image_transport republish compressed in:=/usb_cam/image_raw raw out:=/usb_cam/image_raw" & \
+bash -c "sleep 4; source /opt/ros/melodic/setup.bash; source /root/catkin_ws/devel/setup.bash; rosbag play -l virtual_uav_qr_code.bag"
